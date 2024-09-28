1860c687afbe58de28cac342e704ad2d45fd76cb
NekoRie03 <bunag.alyssamarie03@gmail.com>
2024-09-28 15:34:02 +0800

    Modified urls.py, views.py & linked htmls on ViolationReview and ModifyViolation

--- Project_admin/Project/adminmod/urls.py
+++ Project_admin/Project/adminmod/urls.py
@@ -4,6 +4,39 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    
+    #dboard_violation_review
+    path('ViolationReportsActive', views.ViolationReportsActive, name='ViolationReportsActive'),
+    path('ViolationReportsWarning', views.ViolationReportsWarning, name='ViolationReportsWarning'),
+    path('ViolationReportsProbation', views.ViolationReportsProbation, name='ViolationReportsProbation'),
+    path('ViolationReportsSuspension', views.ViolationReportsSuspension, name='ViolationReportsSuspension'),
+    path('ViolationReportsExpulsion', views.ViolationReportsExpulsion, name='ViolationReportsExpulsion'),
+    path('ViolationReportsInactive', views.ViolationReportsInactive, name='ViolationReportsInactive'),
+    
+    #issue_status
+    path('IssueWarning', views.IssueWarning, name='IssueWarning'),
+    path('IssueProbation', views.IssueProbation, name='IssueProbation'),
+    path('IssueSuspension', views.IssueSuspension, name='IssueSuspension'),
+    path('IssueExpulsion', views.IssueExpulsion, name='IssueExpulsion'),
+    path('DenyReport', views.DenyReport, name='DenyReport'),
+    
+    #summary_report
+    path('SummaryWarning', views.SummaryWarning, name='SummaryWarning'),
+    path('SummaryProbation', views.SummaryProbation, name='SummaryProbation'),
+    path('SummarySuspension', views.SummarySuspension, name='SummarySuspension'),
+    path('SummaryExpulsion', views.SummaryExpulsion, name='SummaryExpulsion'), 
+    
+    #modify_status
+    path('ModifyExpulsion', views.ModifyExpulsion, name='ModifyExpulsion'),
+    path('ModifySuspension', views.ModifySuspension, name='ModifySuspension'),
+    
+    #dboard_modify_violation
+    path('ModifyViolation', views.ModifyViolation, name='ModifyViolation'),
+    path('Infopopup2', views.Infopopup2, name='Infopopup2'),
+    path('AddViolation', views.AddViolation, name='AddViolation'),
+    path('EditViolation', views.EditViolation, name='EditViolation'),
+    
+    #
     path('studrec/', views.studrec, name='studrec'),
     path('studrec2/', views.studrec2, name='studrec2'),
     path("studlist/", views.studlist, name="studlist"),
--- Project_admin/Project/adminmod/views.py
+++ Project_admin/Project/adminmod/views.py
@@ -2,9 +2,65 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 # Create your views here.
-# ------ Student Records ------
+
+# ------ Admin Dashboard ------
 def dashboard(request):
     return render(request,'Dashboard.html')
+# ------ Violation Review ------
+#dboard_violations
+def ViolationReportsActive(request):
+    return render(request, 'dboard_violation_rev/ViolationReportsActive.html')
+def ViolationReportsWarning(request):
+    return render(request, 'dboard_violation_rev/ViolationReportsWarning.html')
+def ViolationReportsProbation(request):
+    return render(request, 'dboard_violation_rev/ViolationReportsProbation.html')
+def ViolationReportsSuspension(request):
+    return render(request, 'dboard_violation_rev/ViolationReportsSuspension.html')
+def ViolationReportsExpulsion(request):
+    return render(request, 'dboard_violation_rev/ViolationReportsExpulsion.html')
+def ViolationReportsInactive(request):
+    return render(request, 'dboard_violation_rev/ViolationReportsInactive.html')
+
+#issue_status dboard_violation_rev/issue_status/ 
+def IssueWarning(request):
+    return render(request, 'dboard_violation_rev/issue_status/IssueWarning.html')
+def IssueProbation(request):
+    return render(request, 'dboard_violation_rev/issue_status/IssueProbation.html')
+def IssueSuspension(request):
+    return render(request, 'dboard_violation_rev/issue_status/IssueSuspension.html')
+def IssueExpulsion(request):
+    return render(request, 'dboard_violation_rev/issue_status/IssueExpulsion.html')
+def DenyReport(request):
+    return render(request, 'dboard_violation_rev/issue_status/DenyReport.html')
+
+#summary_report dboard_violation_rev/summary_report/
+def SummaryWarning(request):
+    return render(request, 'dboard_violation_rev/summary_report/SummaryWarning.html')
+def SummaryProbation(request):
+    return render(request, 'dboard_violation_rev/summary_report/SummaryProbation.html')
+def SummarySuspension(request):
+    return render(request, 'dboard_violation_rev/summary_report/SummarySuspension.html')
+def SummaryExpulsion(request):
+    return render(request, 'dboard_violation_rev/summary_report/SummaryExpulsion.html')
+
+#modify_stat dboard_violation_rev/summary_issue/
+def ModifyExpulsion(request):
+    return render(request, 'dboard_violation_rev/summary_issue/ModifyExpulsion.html')
+def ModifySuspension(request):
+    return render(request, 'dboard_violation_rev/summary_issue/ModifySuspension.html')
+
+# ------ Modify Violation ------
+#dboard_modify
+def ModifyViolation(request):
+    return render(request, 'db_modify_violation/ModifyViolation.html')
+def Infopopup2(request):
+    return render(request, 'db_modify_violation/Infopopup2.html')
+def AddViolation(request):
+    return render(request, 'db_modify_violation/AddViolation.html')
+def EditViolation(request):
+    return render(request, 'db_modify_violation/EditViolation.html')
+
+# ------ Student Records ------
 def studrec(request):
     return render(request, 'Student Records.html')
 def studrec2(request):
