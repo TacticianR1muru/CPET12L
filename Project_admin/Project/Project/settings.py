cb3cb0a2d1c110adfe314ff93da3b75d5c1bc6e6
Christian <166710484+mandubb@users.noreply.github.com>
2024-09-29 18:53:21 +0800

    Add files via upload

--- Project_admin/Project/Project/settings.py
+++ Project_admin/Project/Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adminmod',
 ]
 
 MIDDLEWARE = [
@@ -114,8 +115,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+DEBUG = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
