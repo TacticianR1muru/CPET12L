072b217644c5d22e34c7a14aeb6b04ee122ab60b
zivan <imivanzamora12@gmail.com>
2024-10-30 00:55:52 +0800

    to be continued

--- Project_admin/Project/Project/settings.py
+++ Project_admin/Project/Project/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 DEBUG = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-
+    os.path.join(BASE_DIR, 'adminmod/static'),
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
