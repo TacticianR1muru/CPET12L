4ea0ec859e322d3371d9c633106d599a5c67e630
zivan <imivanzamora12@gmail.com>
2024-11-01 18:34:40 +0800

    "Removed static CSS files for login, registration success, signup, and student status pages, and updated settings.py file."

--- Project_admin/Project/Project/settings.py
+++ Project_admin/Project/Project/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 DEBUG = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-
-]
-# Default primary key field type
+STATICFILES_DIRS = [ ]# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
